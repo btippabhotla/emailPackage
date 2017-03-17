f9acef0e926e7b8457401da8b391002ddb27b567
btippabhotla <bharadwajt10@gmail.com>
2017-03-17 15:46:17 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name emailPackageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the emailPackageApp
+ */
+angular.module('emailPackageApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
